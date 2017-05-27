110e379f75e67b5cbd038de503325eecdedd2044
CXP <q978515742@163.com>
2017-05-27 11:05:26 +0800

    微信支付和支付宝支付

---
+++ app/src/main/java/cn/tddance/tddanceapp/IPayResult.java
@@ -0,0 +1,10 @@
+package cn.tddance.tddanceapp;
+
+/**
+ * Created by Administrator on 2016/11/9.
+ */
+
+public interface IPayResult {
+
+    public void payResult(boolean b, int type);
+}
---
+++ app/src/main/java/cn/weixinpay/sdk/pay/Constants.java
@@ -0,0 +1,20 @@
+package cn.weixinpay.sdk.pay;
+
+public class Constants {
+
+    //是否微信登录
+    public static boolean isWXLogin = false;
+
+    //appid
+    //请同时修改  AndroidManifest.xml里面，.MainActivity里的属性<data android:scheme="XXXXXXXXXXXXXXXXX"/>为新设置的appid
+    public static final String APP_ID = "XXXXXXXXXXXXXXXXX";
+
+    //商户号
+    public static final String MCH_ID = "XXXXXXXXXXXXXXXXX";
+
+    //API密钥，在商户平台设置
+    public static final String API_KEY = "XXXXXXXXXXXXXXXXX";
+
+    public static final String APP_SECRET = "XXXXXXXXXXXXXXXXX";
+
+}
